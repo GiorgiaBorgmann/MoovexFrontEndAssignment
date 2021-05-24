9e5b24ed08a033f8d64e430f5dfa1ca7967789e6
Giorgia Borgmann <giorgia.borgmann@data-lagoon.com>
2021-05-24 16:06:37 +0300

    Add interceptor to the HTTP requests

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import axios from 'axios'
+
+(function () {
+  let token = localStorage.getItem('token');
+  if (token) {
+    axios.defaults.headers.common['Token'] = token;
+  } else {
+    axios.defaults.headers.common['Token'] = null;
+  }
+})();
 
 ReactDOM.render(
   <React.StrictMode>
